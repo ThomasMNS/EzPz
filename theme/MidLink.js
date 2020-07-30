f415a7b86355d6fa8c932de97156c413c9e915dc
ThomasMNS <thomasmns@outlook.com>
2020-07-30 17:56:51 +0100

    Mid Link

---
+++ theme/MidLink.js
@@ -0,0 +1,16 @@
+var bannerLink = document.createElement('a');
+bannerLink.href = "http://bit.ly/ezpz-brave"
+bannerLink.target = "_blank"
+
+var banner = document.createElement('IMG');
+banner.src = 'MidBanner.png';
+banner.id = "mid-banner";
+
+bannerLink.appendChild(banner)
+
+var mainContent = document.getElementById("page-content");
+var contentParas = mainContent.getElementsByTagName('P')
+var numberOfParas = contentParas.length
+if (numberOfParas >= 6) {
+	mainContent.insertBefore(bannerLink, contentParas[3].nextSibling)
+}
