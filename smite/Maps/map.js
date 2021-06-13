c4f1c2c97298b0faa0565ad3ea2a190951c0fb94
ThomasMNS <thomasmns@outlook.com>
2021-06-13 17:44:10 +0100

    Map test

--- smite/Maps/map.js
+++ smite/Maps/map.js
@@ -1,11 +1,12 @@
 var bounds = [[11022.556901, -8277.510648], [-11012.443099, 8672.489352]];
 var image = L.imageOverlay('Maps/Conquest Map.png', bounds);
+var kills = geoJSON("Maps/CamelotKingsKills.json")
 
 var map = L.map('mapid', {
     crs: L.CRS.Simple,
     minZoom: -50, 
     maxZoom: 50,
-    layers: [image]
+    layers: [image, kills]
 });
 
 map.fitBounds(bounds);
