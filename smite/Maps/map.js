86a7acd60d52ad605b8c4b2b71f331a1505fca0e
ThomasMNS <thomasmns@outlook.com>
2021-06-13 17:24:32 +0100

    Map test

--- smite/Maps/map.js
+++ smite/Maps/map.js
@@ -1,4 +1,4 @@
-var bounds = [[-8277.510648, 11022.556901], [8672.489352, -11012.443099]];
+var bounds = [[11022.556901, -8277.510648], [-11012.443099, 8672.489352]];
 var image = L.imageOverlay('Maps/Conquest Map.png', bounds);
 
 var map = L.map('mapid', {
